221c90353824a5da4ac9c7dd5303b38213ad9fd9
vijayabharathi <vijayabharathib@users.noreply.github.com>
2018-05-05 15:22:56 +0530

    add auth0 login functionality as per part 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root 'home#show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "home#show"
+  get "/auth/oauth2/callback" => "auth0#callback"
+  get "/auth/failure" => "auth0#failure"
 end
